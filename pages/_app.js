fd44aebec96f4739f295714c3cd560b5511f26ba
zequnyu <zequnyu11@gmail.com>
2018-12-28 20:33:28 -0800

    fix page loading speed problem

--- pages/_app.js
+++ pages/_app.js
@@ -18,13 +18,6 @@ export default class MyApp extends App {
         return <Container>
             <Head>
                 <title>uRank</title>
-                <link href="https://fonts.googleapis.com/css?family=Open+Sans|Pacifico|Montserrat:500" rel="stylesheet" />
-                <link
-                    rel="stylesheet"
-                    href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
-                    integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
-                    crossOrigin="anonymous"
-                />
                 <link rel="icon" href="/static/favicon.ico" />
             </Head>
             <Component {...pageProps} />
