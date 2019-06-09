86ad01f567631b9146342c39fe01ffef49b3ccba
zequnyu <zequnyu11@gmail.com>
2019-06-09 15:36:10 -0700

    active nav burger toggle

--- urank/src/components/Navbar.js
+++ urank/src/components/Navbar.js
@@ -7,32 +7,53 @@ import 'bulma/css/bulma.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
 
-function Navbar(props) {
-    return (
-        <nav className="navbar is-light is-fixed-top">
-            <div className="container">
-                <div className="navbar-brand">
-                    <a className="navbar-item" href="/">
-                        <img src={blackLogo} alt="Black Logo" />
-                    </a>
-                    <span className="navbar-burger burger" data-target="navbarMenuHeroA">
+class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            burgerActive: false
+        };
+        this.handleBurgerClick = this.handleBurgerClick.bind(this);
+    }
+
+
+    handleBurgerClick() {
+        this.setState({burgerActive: !this.state.burgerActive})
+    }
+
+    render() {
+        console.log(this.state.burgerActive);
+        return (
+            <nav className="navbar is-light is-fixed-top">
+                <div className="container">
+                    <div className="navbar-brand">
+                        <a className="navbar-item" href="/">
+                            <img src={blackLogo} alt="Black Logo" />
+                        </a>
+                        <span className={this.state.burgerActive ? "navbar-burger burger is-active" :
+                            "navbar-burger burger"}
+                              data-target="navbarMenuHeroA"
+                              onClick={this.handleBurgerClick}
+                        >
                         <span></span>
                         <span></span>
                         <span></span>
                     </span>
-                </div>
-                <div id="navbarMenuHeroA" className="navbar-menu">
-                    <div className="navbar-end">
-                        <a className="navbar-item" href="/">
-                            Home
-                        </a>
-                        <a className="navbar-item" href="/">
-                            About
-                        </a>
-                        <span className="navbar-item">
-                            {props.input}
+                    </div>
+                    <div id="navbarMenuHeroA"
+                         className={this.state.burgerActive ? "navbar-menu is-active": "navbar-menu"}
+                    >
+                        <div className="navbar-end">
+                            <a className="navbar-item" href="/">
+                                Home
+                            </a>
+                            <a className="navbar-item" href="/">
+                                About
+                            </a>
+                            <span className="navbar-item">
+                            {this.props.input}
                         </span>
-                        <span className="navbar-item">
+                            <span className="navbar-item">
                             <a className="button is-dark" href="https://github.com/zequnyu/urank" target="_blank"
                                rel="noopener noreferrer">
                                 <span className="icon">
@@ -41,11 +62,12 @@ function Navbar(props) {
                                 <span>Github</span>
                             </a>
                         </span>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </nav>
-    );
+            </nav>
+        );
+    }
 }
 
 Navbar.propTypes = {
